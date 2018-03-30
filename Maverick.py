798ea8c1b243eb3a58d4e0ce4d58908a9dacb3ca
After Packet <Jordan@Lassiter.eu>
2018-03-30 15:00:47 -0400

    Add files via upload

---
+++ Maverick.py
@@ -0,0 +1,31 @@
+# /usr/bin/env Python
+# -*- coding: utf-8 -*-
+import sys
+from time import sleep
+import thread
+import os
+import signal
+#Maverick
+#Created By: AfterPacket
+#www.afterpacket.pw
+#Description: This will use several methods to create a amplifed  ddos attack against the intended #target
+ 
+
+
+print(r"""\
+ _______  _______           _______  _______ _________ _______  _       
+(       )(  ___  )|\     /|(  ____ \(  ____ )\__   __/(  ____ \| \    /\
+| () () || (   ) || )   ( || (    \/| (    )|   ) (   | (    \/|  \  / /
+| || || || (___) || |   | || (__    | (____)|   | |   | |      |  (_/ / 
+| |(_)| ||  ___  |( (   ) )|  __)   |     __)   | |   | |      |   _ (  
+| |   | || (   ) | \ \_/ / | (      | (\ (      | |   | |      |  ( \ \ 
+| )   ( || )   ( |  \   /  | (____/\| ) \ \_____) (___| (____/\|  /  \ \
+|/     \||/     \|   \_/   (_______/|/   \__/\_______/(_______/|_/    \/
+
+
+Stresser Tool 	
+Created By: AfterPacket wwww.afterpacket.pw
+                                                                                        """)
+
+
+Tsrget=raw_input("Enter IP or Domain Name: " ) 
